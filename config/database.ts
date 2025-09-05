e41cd7ffdc54183fffbf83c0a516365e409ba67c
akhancode <akhan.code@gmail.com>
2025-09-05 17:28:20 +0530

    changed to sqlite

--- config/database.ts
+++ config/database.ts
@@ -1,80 +1,25 @@
 import path from "path";
 
-// export default ({ env }) => {
-//   const client = env("DATABASE_CLIENT", "sqlite");
-
-//   const connections = {
-//     mysql: {
-//       client: env("DATABASE_CLIENT", "postgres"),
-
-//       connection: {
-//         connectionString: env("DATABASE_URL"),
-//         ssl: {
-//           rejectUnauthorized: false, // Required for Neon
-//         },
-//       },
-//       pool: {
-//         min: env.int("DATABASE_POOL_MIN", 2),
-//         max: env.int("DATABASE_POOL_MAX", 10),
-//       },
-//     },
-//     postgres: {
-//       connection: {
-//         connectionString: env("DATABASE_URL"),
-//         host: env("DATABASE_HOST", "localhost"),
-//         port: env.int("DATABASE_PORT", 5432),
-//         database: env("DATABASE_NAME", "strapi"),
-//         user: env("DATABASE_USERNAME", "strapi"),
-//         password: env("DATABASE_PASSWORD", "strapi"),
-//         ssl: env.bool("DATABASE_SSL", false) && {
-//           key: env("DATABASE_SSL_KEY", undefined),
-//           cert: env("DATABASE_SSL_CERT", undefined),
-//           ca: env("DATABASE_SSL_CA", undefined),
-//           capath: env("DATABASE_SSL_CAPATH", undefined),
-//           cipher: env("DATABASE_SSL_CIPHER", undefined),
-//           rejectUnauthorized: env.bool(
-//             "DATABASE_SSL_REJECT_UNAUTHORIZED",
-//             true
-//           ),
-//         },
-//         schema: env("DATABASE_SCHEMA", "public"),
-//       },
-//       pool: {
-//         min: env.int("DATABASE_POOL_MIN", 2),
-//         max: env.int("DATABASE_POOL_MAX", 10),
-//       },
-//     },
-//     sqlite: {
-//       connection: {
-//         filename: path.join(
-//           __dirname,
-//           "..",
-//           "..",
-//           env("DATABASE_FILENAME", ".tmp/data.db")
-//         ),
-//       },
-//       useNullAsDefault: true,
-//     },
-//   };
-
-//   return {
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
 //     connection: {
-//       client,
-//       ...connections[client],
-//       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//       connectionString: env("DATABASE_URL"),
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
 //     },
-//   };
-// };
+//     useNullAsDefault: true,
+//   },
+// });
 
+// config/database.ts
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      connectionString: env("DATABASE_URL"),
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+      filename: env("DATABASE_FILENAME", "./data/data.db"),
     },
     useNullAsDefault: true,
   },
