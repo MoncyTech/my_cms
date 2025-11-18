312928cf532e67bba86b24f3d981157549c7486f
akhancode <akhan.code@gmail.com>
2025-11-18 15:05:20 +0530

    some changes

--- seed-db.js
+++ seed-db.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const volumeDb = "/app/data/data.db"; // Railway volume path
-const repoDb = __dirname + "/data/data.db"; // your repo DB path (adjust if needed)
+const repoDb = __dirname + "/data-seed/data.db"; // your repo DB path (adjust if needed)
 
 if (!fs.existsSync(volumeDb)) {
   console.log("Seeding DB into Railway volume...");
